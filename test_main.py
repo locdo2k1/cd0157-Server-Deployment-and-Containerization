ec5ab9461269969d14cb297714cf52b84380049c
Loc Do <locdoduc2k1@gmail.com>
2024-07-02 20:11:59 +0700

    complete code project 4

--- test_main.py
+++ test_main.py
@@ -26,6 +26,7 @@ def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
+    assert False, "This test intentionally fails"
 
 
 def test_auth(client):
